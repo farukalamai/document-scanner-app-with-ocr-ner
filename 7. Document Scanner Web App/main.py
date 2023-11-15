0117a3453ac4cd0888a7a7b047339058d5aafa46
farukalampro <mdfarukt222@gmail.com>
2023-11-15 06:20:01 +0600

    doc app

--- 7. Document Scanner Web App/main.py
+++ 7. Document Scanner Web App/main.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template
 import settings
 import utils
+import numpy as np
+import cv2
 
 
 app = Flask(__name__)
@@ -18,10 +20,50 @@ def scandoc():
         # predict the coordinate the document
         four_points, size = docscan.document_scanner(upload_image_path)
         print(four_points, size)
-        
+        if four_points is None:
+            message = "Unable to locate the corodinate of document"
+            points = [
+                {'x': 10, 'y': 10},
+                {'x': 120, 'y': 10},
+                {'x': 120, 'y': 120},
+                {'x': 10, 'y': 120}
+            ]
+            return render_template('scanner.html', 
+                                    points=points,
+                                    fileupload = True,
+                                    message = message
+                                    )
+        else:
+            points = utils.array_to_json_format(four_points)
+            message = "Located the Corridinates of Document using OPenCv"
+            return render_template('scanner.html',
+                                    points=points,
+                                    fileupload = True,
+                                    message = message)
+            
         return render_template('scanner.html')
     
     return render_template('scanner.html')
+
+@app.route('/transform', methods=['POST'])
+def transform():
+    try:
+        points = request.json['data']
+        array = np.array(points)
+        magic_color = docscan.calibrate_to_original_size(array)
+        # utils.save_image(magic_color, 'magic_color.jpg')
+
+        filename = 'magic_color.jpg'
+        magic_image_path = settings.join_path(settings.MEDIA_DIR, filename)
+        cv2.imwrite(magic_image_path, magic_color)
+        
+        return 'sucess'
+    except:
+        return 'fail'
+
+@app.route("/prediction")
+def prediction():
+    return 'successfully wrap the image'
 
 @app.route('/about')
 def about():
